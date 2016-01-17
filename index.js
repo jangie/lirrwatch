d835ce4cb81b154fdcfcc4fbea5111720f927c63
Bruce Lee <jangsoo.lee@gmail.com>
2016-01-17 08:06:56 -0500

    cache by serviceId, allow day rollover

--- index.js
+++ index.js
@@ -8,11 +8,20 @@ var Q = require("q")
 var routes = {};
 var trips = {};
 var goodTrips = {};
-var sortedGoodTrips = [];
+var sortedGoodTrips = {};
 var calendar = {};
 var stops = {};
-var lastServiceId = '';
+var stopIdReverseLookup = {};
+
 var fetchGTFSFromMTA = function(){
+  //cleanup
+  routes = {};
+  trips = {};
+  goodTrips = {};
+  sortedGoodTrips = {};
+  calendar = {};
+  stops = {};
+  stopIdReverseLookup = {};
   var deferment = Q.defer();
   if (!fs.existsSync('./gtfsdata')){
     fs.mkdirSync('./gtfsdata');
@@ -67,10 +76,13 @@ var populateRoutes = function(){
       });
 };
 
-var populateTrips = function(routeId, serviceId){
+var populateTrips = function(routeId){
   return populateObjectFromFile('./gtfsdata/trips.txt', function(record){
-    if (record[0] === routeId && record[1] == serviceId){
-      trips[record[2]] = {direction: record[5]};
+    if (!trips[record[1]]){
+      trips[record[1]] = {};
+    }
+    if (record[0] === routeId ){
+      trips[record[1]][record[2]] = {direction: record[5]};
     }
   });
 };
@@ -78,6 +90,7 @@ var populateTrips = function(routeId, serviceId){
 var populateStops = function(){
   return populateObjectFromFile('./gtfsdata/stops.txt', function(record){
     stops[record[1].toUpperCase()] = {id: record[0], friendlyName: record[1]};
+    stopIdReverseLookup[record[0]] = record[1];
   });
 };
 
@@ -87,37 +100,50 @@ var isFirstValueEarlier = function(firstValue, secondValue){
 
 var populateStopTimes = function(firstStationId, secondStationId){
   tripsChecker = {};
+  var tripIds = {};
+  for (var serviceId in trips){
+    for (var tripId in trips[serviceId]){
+      tripIds[tripId] = serviceId;
+    }
+  }
   return populateObjectFromFile('./gtfsdata/stop_times.txt', function(record){
-    if (trips[record[0]]){
-      if (!tripsChecker[record[0]]){
-        tripsChecker[record[0]] = {};
+    if (tripIds[record[0]]){
+      var stopServiceId = tripIds[record[0]];
+      if (!tripsChecker[stopServiceId]){
+        tripsChecker[stopServiceId] = {};
+      }
+      if (!tripsChecker[stopServiceId][record[0]]){
+        tripsChecker[stopServiceId][record[0]] = {};
+      }
+      if (!goodTrips[stopServiceId]){
+        goodTrips[stopServiceId] = {};
       }
       if (record[3] === firstStationId){
-        tripsChecker[record[0]][firstStationId] = record[1];
-        if (tripsChecker[record[0]][secondStationId]){
-          goodTrips[record[0]] = {};
+        tripsChecker[stopServiceId][record[0]][firstStationId] = record[1];
+        if (tripsChecker[stopServiceId][record[0]][secondStationId]){
+          goodTrips[stopServiceId][record[0]] = {};
         }
       }
       if (record[3] === secondStationId){
-        tripsChecker[record[0]][secondStationId] = record[1];
-        if (tripsChecker[record[0]][firstStationId]){
-          goodTrips[record[0]] = {};
+        tripsChecker[stopServiceId][record[0]][secondStationId] = record[1];
+        if (tripsChecker[stopServiceId][record[0]][firstStationId]){
+          goodTrips[stopServiceId][record[0]] = {};
         }
       }
-      if (goodTrips[record[0]]){
-        goodTrips[record[0]] = tripsChecker[record[0]];
+      if (goodTrips[stopServiceId][record[0]]){
+        goodTrips[stopServiceId][record[0]] = tripsChecker[stopServiceId][record[0]];
         if (isFirstValueEarlier(
-          goodTrips[record[0]][firstStationId],
-          goodTrips[record[0]][secondStationId])){
-          goodTrips[record[0]].departStation = firstStationId;
-          goodTrips[record[0]].departTime = goodTrips[record[0]][firstStationId];
-          goodTrips[record[0]].arriveStation = secondStationId;
-          goodTrips[record[0]].arriveTime = goodTrips[record[0]][secondStationId];
+          goodTrips[stopServiceId][record[0]][firstStationId],
+          goodTrips[stopServiceId][record[0]][secondStationId])){
+          goodTrips[stopServiceId][record[0]].departStation = firstStationId;
+          goodTrips[stopServiceId][record[0]].departTime = goodTrips[stopServiceId][record[0]][firstStationId];
+          goodTrips[stopServiceId][record[0]].arriveStation = secondStationId;
+          goodTrips[stopServiceId][record[0]].arriveTime = goodTrips[stopServiceId][record[0]][secondStationId];
         }else{
-          goodTrips[record[0]].departStation = secondStationId;
-          goodTrips[record[0]].departTime = goodTrips[record[0]][secondStationId];
-          goodTrips[record[0]].arriveStation = firstStationId;
-          goodTrips[record[0]].arriveTime = goodTrips[record[0]][firstStationId];
+          goodTrips[stopServiceId][record[0]].departStation = secondStationId;
+          goodTrips[stopServiceId][record[0]].departTime = goodTrips[stopServiceId][record[0]][secondStationId];
+          goodTrips[stopServiceId][record[0]].arriveStation = firstStationId;
+          goodTrips[stopServiceId][record[0]].arriveTime = goodTrips[stopServiceId][record[0]][firstStationId];
         }
       }
     }
@@ -131,20 +157,28 @@ var getTwoDigitValue = function(input){
   return "" + input;
 };
 
+var getYYYYMMDD = function(desiredDateObject){
+  return "" + desiredDateObject.getFullYear() +
+    getTwoDigitValue(desiredDateObject.getMonth() + 1) +
+    getTwoDigitValue(desiredDateObject.getDate())
+}
 
 var nextSchedules = module.exports.nextSchedules =
   function (numberOfSchedules, routeName, firstStationName, secondStationName){
+    var nextSchedulesDeferment = Q.defer();
     var calendarPopulated = Q("Done");
     var now = new Date();
-    var formattedDate = "" + now.getFullYear() + getTwoDigitValue(now.getMonth() + 1) + getTwoDigitValue(now.getDate());
-    var route, serviceId, firstStationId, secondStationId;
-    var stopIdReverseLookup = {};
-    if (!calendar[formattedDate]){
+    var tomorrow = new Date(now.getTime + 24*60*60*1000);
+    var formattedDate = getYYYYMMDD(now);
+    var tomorrowFormattedDate = getYYYYMMDD(tomorrow);
+    var route, serviceId, tomorrowServiceId, firstStationId, secondStationId;
+    if (!calendar[formattedDate] || !calendar[tomorrowFormattedDate]){
       calendarPopulated = populateCalendar();
     }
     calendarPopulated.then(function(){
       serviceId = calendar[formattedDate];
-      var routesPopulated = Q("Done");
+      tomorrowServiceId = calendar[tomorrowFormattedDate];
+      var routesPopulated = "Done";
       if (Object.keys(routes).length === 0){
         routesPopulated = populateRoutes();
       }
@@ -154,11 +188,10 @@ var nextSchedules = module.exports.nextSchedules =
         throw Error("Badness");
       }
       route = routes[routeName.toUpperCase()];
-      if (serviceId !== lastServiceId){
-        trips = {};
-        return populateTrips(route.id, serviceId);
+      if (!trips[serviceId] || !trips[tomorrowServiceId]){
+        return populateTrips(route.id);
       }
-      return Q("Done");
+      return "Done";
     }).then(function(){
       if (!stops[firstStationName.toUpperCase()] || !stops[secondStationName.toUpperCase()]){
         return populateStops();
@@ -168,54 +201,67 @@ var nextSchedules = module.exports.nextSchedules =
       }).then(function(){
         firstStationId = stops[firstStationName.toUpperCase()].id;
         secondStationId = stops[secondStationName.toUpperCase()].id;
-        stopIdReverseLookup[firstStationId] = firstStationName;
-        stopIdReverseLookup[secondStationId] = secondStationName;
-        if (serviceId !== lastServiceId){
+        if (!sortedGoodTrips[serviceId] || !sortedGoodTrips[tomorrowServiceId]){
           goodTrips = {};
           sortedGoodTrips = [];
           return populateStopTimes(firstStationId, secondStationId);
         }
         return "Done";
       }).then(function(){
-        lastServiceId = serviceId;
         var currentTime = getTwoDigitValue(now.getHours()) + ":" +
           getTwoDigitValue(now.getMinutes()) + ":" +
           getTwoDigitValue(now.getSeconds());
-        for (var tripId in goodTrips){
-          sortedGoodTrips.push({tripId: tripId, data: goodTrips[tripId]});
+        if (!sortedGoodTrips[serviceId]){
+          for (var serviceIdKey in goodTrips){
+            sortedGoodTrips[serviceIdKey] = [];
+            for (var tripId in goodTrips[serviceIdKey]){
+              sortedGoodTrips[serviceIdKey].push({tripId: tripId, data: goodTrips[serviceIdKey][tripId]});
+            }
+            //this is just to work around bad sorts in gtfs.
+            sortedGoodTrips[serviceIdKey].sort(
+              function(a, b){
+                if (a.data.departTime > b.data.departTime){
+                  return 1;
+                }
+                if (a.data.departTime < b.data.departTime){
+                  return -1;
+                }
+                return 0;
+              });
+          }
         }
-        //this is just to work around bad sorts in gtfs.
-        sortedGoodTrips.sort(
-          function(a, b){
-            if (a.data.departTime > b.data.departTime){
-              return 1;
-            }
-            if (a.data.departTime < b.data.departTime){
-              return -1;
-            }
-            return 0;
-          });
-        var foundBits = 0;
-        for (var i = 0; i < sortedGoodTrips.length; i++){
-          var testedTrip = sortedGoodTrips[i];
-          if (testedTrip.data.departTime > currentTime || testedTrip.data.arriveTime > currentTime){
-            foundBits++;
-            console.log(stopIdReverseLookup[testedTrip.data.departStation]
-              +
-              ":" + testedTrip.data.departTime +
-            "  ->  " + stopIdReverseLookup[testedTrip.data.arriveStation] + ":" + testedTrip.data.arriveTime);
-            if (foundBits === numberOfSchedules){
-              break;
+        var foundBits = [];
+        var addToFoundBits = function(serviceIdToCheck){
+          for (var i = 0; i < sortedGoodTrips[serviceIdToCheck].length; i++){
+            var testedTrip = sortedGoodTrips[serviceIdToCheck][i];
+            if (testedTrip.data.departTime > currentTime || testedTrip.data.arriveTime > currentTime){
+              foundBits.push(testedTrip);
+              if (foundBits.length === numberOfSchedules){
+                break;
+              }
             }
           }
         }
+        addToFoundBits(serviceId);
+        if (!foundBits.length === numberOfSchedules){
+          addToFoundBits(tomorrowServiceId);
+        }
+        nextSchedulesDeferment.resolve(foundBits);
         });
-
+    return nextSchedulesDeferment.promise;
   }
 ;
 
-//TODO: Account for not finding enough trips in current day. Will require restructuring.
 //TODO: Account for realtime GTFS.
 //TODO: Determine how best to make 12H display.
 
-nextSchedules(5, "Port Washington", "Great Neck", "Penn Station");
+nextSchedules(5, "Port Washington", "Great Neck", "Penn Station").then(function(result){
+  for (var i = 0; i < result.length; i++){
+    var goodTrip = result[i];
+    console.log(stopIdReverseLookup[goodTrip.data.departStation]
+      +
+      ":" + goodTrip.data.departTime +
+      "  ->  " + stopIdReverseLookup[goodTrip.data.arriveStation] + ":" + goodTrip.data.arriveTime);
+  }
+
+});
